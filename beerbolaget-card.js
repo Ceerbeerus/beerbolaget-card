717972ee8cc7ed6cc87a5e310cd7436d8bfd4ad8
Ceerbeerus <46457478+Ceerbeerus@users.noreply.github.com>
2019-09-02 11:10:22 +0200

    Update beerbolaget-card.js

--- beerbolaget-card.js
+++ beerbolaget-card.js
@@ -230,7 +230,7 @@ class BeerbolagetCard extends HTMLElement {
             if (breweryChecks[0] === 'the') {
                 breweryChecks.shift();
             }
-            if (detailedName && (name.toLowerCase() === breweryChecks[0]) ||
+            if (detailedName && (name.toLowerCase() === breweryChecks[0])) ||
                    (breweryChecks.length > 1 &&
                     name.toLowerCase().includes(breweryChecks[1]))) &&
                         (!detailedName.toLowerCase().includes(breweryChecks[0]))) {
